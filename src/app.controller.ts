cd2d7a886c13ab67b9af53a08a6436dc8eda5c1d
Kevin Ortiz <92760727+KevinorJG@users.noreply.github.com>
2025-08-19 23:51:43 +0000

    Agregando healtcheck basico, para verificar servicio

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,13 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("healt")
+  healtCheck(){
+    return {
+      status: 'ok',
+      timestamp: new Date().toISOString(),
+    };
+  }
+
 }
